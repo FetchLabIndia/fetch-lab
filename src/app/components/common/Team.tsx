764764b4472c519b8b6fd1be70f4384136df64a1
Zade Scoxp <78777405+zadescoxp@users.noreply.github.com>
2024-09-30 23:38:52 +0530

    minor design change

--- src/app/components/common/Team.tsx
+++ src/app/components/common/Team.tsx
@@ -4,9 +4,9 @@ function Team() {
       <p className="font-extrabold text-4xl leading-ms tracking-ms ">
         Meet the <span className=" text-cuGreen">Visionaries</span>
       </p>
-      <div className=" flex max-sm:flex-wrap items-center gap-10 text-base justify-center">
+      <div className=" flex items-center gap-5 text-base justify-center max-sm:flex-wrap max-sm:px-2">
         <div>
-          <div className=" w-[200px] h-[203px] max-sm:w-[170px] max-sm:h-[173px] border rounded-2xl"></div>
+          <div className=" w-[200px] h-[203px] max-sm:w-[140px] max-sm:h-[143px] border rounded-2xl"></div>
           <div className=" mt-2">
             <p className=" font-bold ">Govind Singh Bisht</p>
             <p className=" font-sofiaSans  -mt-1 text-sm">
@@ -15,7 +15,7 @@ function Team() {
           </div>
         </div>
         <div>
-          <div className=" w-[200px] h-[203px] max-sm:w-[170px] max-sm:h-[173px] border rounded-2xl"></div>
+          <div className=" w-[200px] h-[203px] max-sm:w-[140px] max-sm:h-[143px] border rounded-2xl"></div>
           <div className=" mt-2">
             <p className=" font-bold ">Govind Singh Bisht</p>
             <p className=" font-sofiaSans -mt-1 text-sm">
@@ -24,7 +24,7 @@ function Team() {
           </div>
         </div>
         <div>
-          <div className=" w-[200px] h-[203px] max-sm:w-[170px] max-sm:h-[173px] border rounded-2xl"></div>
+          <div className=" w-[200px] h-[203px] max-sm:w-[140px] max-sm:h-[143px] border rounded-2xl"></div>
           <div className=" mt-2">
             <p className=" font-bold ">Govind Singh Bisht</p>
             <p className=" font-sofiaSans  -mt-1 text-sm">
