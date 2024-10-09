dbefb2d7f844f4759b9034f5499748fc7e9515e9
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-10-09 12:48:44 +0530

    added  mail

--- src/app/api/mail/route.ts
+++ src/app/api/mail/route.ts
@@ -1,26 +1,28 @@
-import { NextRequest, NextResponse } from "next/server";
-import nodemailer from "nodemailer";
-import SMTPTransport from "nodemailer/lib/smtp-transport";
-const transport = nodemailer.createTransport({
-  service: "gmail",
-  //   secure: process.env.NODE_ENV !== "development",
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASSWORD,
-  },
-} as SMTPTransport.Options);
+import { NextResponse } from "next/server";
+// import nodemailer from "nodemailer";
+// import SMTPTransport from "nodemailer/lib/smtp-transport";
 
-export async function POST(req: NextRequest) {
+export async function POST() {
   try {
-    const { sender } = await req.json();
-    await transport.sendMail({
-      from: "your-email@example.com",
-      to: "hello@fetchlab.in",
-      subject: "New Contact Form Submission",
-      html: `Sender: ${sender}`,
-    });
+    // const { sender } = await req.json();
+    // const transport = nodemailer.createTransport({
+    //   service: "gmail",
+    //   auth: {
+    //     user: process.env.MAIL_USER,
+    //     pass: process.env.MAIL_PASSWORD,
+    //   },
+    // } as SMTPTransport.Options);
+
+    // await transport.sendMail({
+    //   from: "your-email@example.com",
+    //   to: "hello@fetchlab.in",
+    //   subject: "New Contact Form Submission",
+    //   html: `Sender: ${sender}`,
+    // });
     return NextResponse.json({}, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
 }
+
+export const runtime = "edge";
