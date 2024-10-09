de703251c20583fef0ebf5c39006d222bdd97727
Tanmay <144552425+babyo77@users.noreply.github.com>
2024-10-09 13:07:18 +0530

    added  mail

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    MAIL_USER: "yfw111realone@gmail.com",
-    MAIL_PASSWORD: "htzr hucb vnab bius",
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
